7e00fcc3b8a3f9ab4c8eb5f9f1bed21588974526
cchifang <145099083+cchifang@users.noreply.github.com>
2023-10-27 14:34:29 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #該如何讀取xml檔案
 import xml.etree.ElementTree as ET
-#匯入函式庫
 def compound_interest(x,s,y):
     total = 0 #計算複利的總和
     for i in range(y):#總共y年
@@ -8,19 +7,15 @@ def compound_interest(x,s,y):
         total = total*(1+s/100)#複利
     return total
 # 解析XML文件
-
-tree = ET.parse("setting.xml")#讀取setting.xml的檔案
-root = tree.getroot()#建立樹結構
-#如何把這個樹結構轉化成python可以使用的資料?
-# 建立一个空字典:在python裡類似樹結構的東西，都是用{}做建立
-
-data_dict = {}#data = {key : value}
-
+tree = ET.parse("setting.xml")
+root = tree.getroot()
+# 创建一个空字典
+data_dict = {}
 # 遍历XML元素并将其存储在字典中
 for element in root:
-    key = element.tag  # 讀取tag:x讀取tag:x、s、y
-    value = element.text  # 讀取裡面的文字 10000、10、20(被tag包夾的文字)
-    data_dict[key] = value # 把資料存到python裡的字典處理
+    key = element.tag  # 元素的标签作为字典的键
+    value = element.text  # 元素的文本内容作为字典的值
+    data_dict[key] = value
 # 打印字典
 print(data_dict)
 x = int(data_dict['x'])
@@ -30,18 +25,18 @@ y = int(data_dict['y'])
 # s = int(input("年利率"))
 # y = int(input("年份"))
 
-print(compound_interest(x,s,y))#算出成果
+print(compound_interest(x,s,y))
 #不但要計算出成果且要存至記事本
 #打開記事本檔案
 file = open("result.txt","w",encoding="utf-8")
 #"result.txt" -> 檔案名稱
-#"w" -> writev寫入
-#encoding -> 文字編碼 繁體用的是"utf-8" 簡體"big-5"
+#"w" -> write寫入
+#encoding ->文字編碼 繁體用的是"utf-8" 簡體"big-5"
 file.write("本金:"+str(x)+"\n")
 file.write("年利率:"+str(s)+"\n")
-file.write("投資:"+str(y)+"\n")
-file.write("---------以下是您可以得到的總金額-----------\n")
-# file.write(str(int(compound_interest(x,s,y)))+".00元")
+file.write("投資年分:"+str(y)+"\n")
+file.write("------以下是您可以得到的總金額------\n")
+#file.write(str(compound_interest(x,s,y)))+".00元")
 num = compound_interest(x,s,y)
 file.write(f"{num:.2f}元") #字串格式化
 file.close()
